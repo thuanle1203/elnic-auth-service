4618e725e52e116bcba29733ea5128c9d4c41d7b
ThuanLX <thuan.le@botstar.com>
2021-11-09 21:37:22 +0700

    fix cors

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ var corsOptions = {
   origin: "http://localhost:4200",
 };
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
