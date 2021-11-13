42719651244aca3d07ca1e93634a92ad772301b0
ThuanLX <thuan.le@botstar.com>
2021-11-13 11:49:29 +0700

    fix-corx

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next()
 });
-
 
 app.use(cors());
 
