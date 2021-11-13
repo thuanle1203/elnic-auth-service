19ef00d8b40eb63685ac9715a27c2ca9554af7b1
ThuanLX <thuan.le@botstar.com>
2021-11-13 11:59:55 +0700

    fix-corx-1

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ app.all('/', function(req, res, next) {
   next()
 });
 
-app.use(cors());
+app.use(cors(corsOptions));
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
