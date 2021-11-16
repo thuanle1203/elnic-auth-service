5566c810e40e20feef1ce774ddae7a61adf3492d
ThuanLX <thuan.le@botstar.com>
2021-11-16 22:23:50 +0700

    feat/thuan_add-UD-for-user

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -3,9 +3,25 @@ const mongoose = require("mongoose");
 const User = mongoose.model(
   "User",
   new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+    username: {
+      type: String,
+      required: true,
+      max: [127, "Max Length is 127 characters"],
+    },
+    email: {
+      type: String,
+      required: true,
+      max: [127, "Max Length is 127 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+      max: [127, "Max Length is 127 characters"],
+    },
+    fullName: String,
+    phone: String,
+    email_verified: Boolean,
+    last_seen: Date,
     roles: [
       {
         type: mongoose.Schema.Types.ObjectId,
